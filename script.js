146b636ab49c56fa329d3bd6b505d162289544c9
Lobu-Touluaan <akunkkt145@gmail.com>
2025-12-10 13:15:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 // Inisialisasi Icon Lucide
 lucide.createIcons();
 
-// Update Tahun di Footer otomatis
-document.getElementById('year').innerText = new Date().getFullYear();
-
 // Fungsi Toggle Menu Mobile
 const mobileMenuBtn = document.getElementById('mobile-menu-btn');
 const mobileMenu = document.getElementById('mobile-menu');
 
-if(mobileMenuBtn){
+if(mobileMenuBtn && mobileMenu){
     mobileMenuBtn.addEventListener('click', () => {
         mobileMenu.classList.toggle('hidden');
     });
